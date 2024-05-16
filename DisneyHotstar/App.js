f8f4ce63710264b59f1adce76e234b66552820d9
Anjana-KA <karanamkoteanjanaka@gmail.com>
2024-05-16 17:52:43 +0530

    Initial Commit

--- DisneyHotstar/App.js
+++ DisneyHotstar/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your ap</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,3 +18,30 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+// import React from 'react';
+// import {StyleSheet, Text, View} from 'react-native';
+
+// const Colors = () => {
+//   return (
+//     <View >
+//       <Text style={styles.red}>just red</Text>
+//       <Text style={styles.bigBlue}>just bigBlue</Text>
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+ 
+//   bigBlue: {
+//     color: 'blue',
+//     fontWeight: 'bold',
+//     fontSize: 30,
+//   },
+//   red: {
+//     color: 'red',
+//   },
+// });
+
+// export default Colors;
